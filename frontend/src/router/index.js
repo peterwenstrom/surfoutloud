3f7615240c80913f74493145671431caf599cd9a
Peter <petme911@student.liu.se>
2017-04-06 01:45:14 +0200

    Added a component and route to figure out how Vue functions

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Bye from '@/components/Bye'
 
 Vue.use(Router)
 
 export default new Router({
+/*  mode: 'history',*/ //Uncomment to remove the hashtag, transition is not smooth though...
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/bye',
+      name: 'Bye',
+      component: Bye
     }
   ]
 })
