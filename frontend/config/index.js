f94a3670b1c5c628cb4f942b719510bc953de46d
Peter <petme911@student.liu.se>
2017-04-06 00:01:47 +0200

    Clean up component and config file

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -30,7 +30,7 @@ module.exports = {
     proxyTable: {
       '/api': {
         target: 'http://localhost:5000',
-          changeOrigin: true
+        changeOrigin: true
       }
     },
     // CSS Sourcemaps off by default because relative paths are "buggy"
