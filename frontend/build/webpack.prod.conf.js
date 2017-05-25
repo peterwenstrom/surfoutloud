113a793145a2a8838cfb2e0b8688f002fe49b33b
Perli337 <perli337@student.liu.se>
2017-05-25 10:37:47 +0200

    added procfile, runtime and serverroute

--- frontend/build/webpack.prod.conf.js
+++ frontend/build/webpack.prod.conf.js
@@ -28,7 +28,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
       'process.env': env,
-      'API_URL': JSON.stringify('add when deployed')
+      'API_URL': JSON.stringify('http://surfoutloudserver.herokuapp.com/')
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
