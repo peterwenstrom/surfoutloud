3ad66d15e230d5c2efaa666a6495a957c56ee379
Perli337 <perli337@student.liu.se>
2017-05-25 13:05:35 +0200

    restore deployment settings

--- frontend/build/webpack.prod.conf.js
+++ frontend/build/webpack.prod.conf.js
@@ -28,7 +28,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
       'process.env': env,
-      'API_URL': JSON.stringify('https://54.243.58.160:5000'),
+      'API_URL': JSON.stringify('https://surfoutloudserver.herokuapp.com'),
       'CHAT_URL': JSON.stringify('wss://surfoutloudserver.herokuapp.com')
     }),
     new webpack.optimize.UglifyJsPlugin({
