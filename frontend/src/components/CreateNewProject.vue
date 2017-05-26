3adcce564a6efbfdc3992d4038e9a324d24b177e
Peter <petme911@student.liu.se>
2017-05-27 00:57:20 +0200

    Add icon to customise project design, just a suggestion

--- frontend/src/components/CreateNewProject.vue
+++ frontend/src/components/CreateNewProject.vue
@@ -34,6 +34,52 @@
     <div class="col-md-6">
       <div class="row">
         <div class="col-md-12">
+          <p><strong>Select Project Icon</strong></p>
+          <div class="project-icon">
+            <label for="one"><icon name="superpowers"></icon></label>
+            <input type="radio" id="one" value="superpowers" v-model="project_details.icon">
+          </div>
+          <div class="project-icon">
+            <label for="two"><icon name="snowflake-o"></icon></label>
+            <input type="radio" id="two" value="snowflake-o" v-model="project_details.icon">
+          </div>
+          <div class="project-icon">
+            <label for="three"><icon name="anchor"></icon></label>
+            <input type="radio" id="three" value="anchor" v-model="project_details.icon">
+          </div>
+          <div class="project-icon">
+            <label for="four"><icon name="heart"></icon></label>
+            <input type="radio" id="four" value="heart" v-model="project_details.icon">
+          </div>
+          <div class="project-icon">
+            <label for="five"><icon name="diamond"></icon></label>
+            <input type="radio" id="five" value="diamond" v-model="project_details.icon">
+          </div>
+          <div class="project-icon">
+            <label for="six"><icon name="hand-spock-o"></icon></label>
+            <input type="radio" id="six" value="hand-spock-o" v-model="project_details.icon">
+          </div>
+          <div class="project-icon">
+            <label for="seven"><icon name="star"></icon></label>
+            <input type="radio" id="seven" value="star" v-model="project_details.icon">
+          </div>
+          <div class="project-icon">
+            <label for="eight"><icon name="paw"></icon></label>
+            <input type="radio" id="eight" value="paw" v-model="project_details.icon">
+          </div>
+          <div class="project-icon">
+            <label for="nine"><icon name="glass"></icon></label>
+            <input type="radio" id="nine" value="glass" v-model="project_details.icon">
+          </div>
+          <div class="project-icon">
+            <label for="ten"><icon name="globe"></icon></label>
+            <input type="radio" id="ten" value="globe" v-model="project_details.icon">
+          </div>
+          <hr class="small">
+        </div>
+
+
+        <div class="col-md-12">
           <label for="add-members"><strong>Add Members</strong></label>
           <input
             id="add-members"
@@ -44,11 +90,10 @@
           >
 
           <div class="icon-div" v-on:click="addMember">
-            <icon name="plus-circle"></icon>
+            <icon id="add-member" name="plus-circle"></icon>
           </div>
         </div>
         <div class="col-md-12">
-          <hr class="small">
           <p><strong>Members that will be added to the project:</strong></p>
         </div>
         <div class="added-member" v-for="member in project_details.new_members">
@@ -81,6 +126,17 @@
   import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
   import Icon from 'vue-awesome/components/Icon'
   import 'vue-awesome/icons/plus-circle'
+  import 'vue-awesome/icons/window-close-o'
+  import 'vue-awesome/icons/superpowers'
+  import 'vue-awesome/icons/snowflake-o'
+  import 'vue-awesome/icons/anchor'
+  import 'vue-awesome/icons/heart'
+  import 'vue-awesome/icons/diamond'
+  import 'vue-awesome/icons/hand-spock-o'
+  import 'vue-awesome/icons/globe'
+  import 'vue-awesome/icons/star'
+  import 'vue-awesome/icons/paw'
+  import 'vue-awesome/icons/glass'
 
   const ADD_PROJECT_URL = API_URL + '/addproject';
 
@@ -92,7 +148,8 @@
           new_members: [],
           name: '',
           description: '',
-          admin: ''
+          admin: '',
+          icon: 'superpowers'
         },
         msg: '',
         member: '',
@@ -149,14 +206,18 @@
     margin-top: 20px;
   }
   .fa-icon {
-    color: #41B883;
+    color: #2973b7;
     cursor: pointer;
-    width: 20px;
+    width: 25px;
     height: auto;
   }
   .icon-div {
     display: inline-block;
     vertical-align: middle;
+  }
+  #add-member {
+    color: #41B883;
+    width: 20px;
   }
   #add-members {
     display: inline-block;
@@ -170,8 +231,23 @@
     text-align: left;
   }
   #cancel {
-    color: #f44250;
+    color: #f66;
+    width: 20px;
     padding-left: 5px;
+  }
+  .project-icon {
+    display: inline-block;
+    margin: 0px 5px 0px 5px;
+  }
+  .project-icon label {
+    display: block;
+    margin-bottom: 0;
+  }
+  .project-icon input {
+    cursor: pointer;
+  }
+  #project-description, #add-members {
+    margin-bottom: 16px;
   }
 
 </style>
