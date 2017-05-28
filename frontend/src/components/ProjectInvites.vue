996e8c20e91b4aa70becde81e3702eb08b835ff9
Peter <petme911@student.liu.se>
2017-05-29 00:01:03 +0200

    Add interface to accept and decline project invites

--- frontend/src/components/ProjectInvites.vue
+++ frontend/src/components/ProjectInvites.vue
@@ -2,10 +2,16 @@
   <div class="col-md-12">
     <hr class="small">
     <h3>Project Invites</h3>
-    <div v-for="project in projects">
-      <p>{{project.name}}</p>
-      <p>{{project.admin}}</p>
-    </div>
+    <transition-group name="fade">
+      <div class="col-md-5 project" v-for="project in projects" v-bind:key="project">
+
+        <div><p><strong>Project name:</strong> <br> {{project.name}}</p></div>
+        <div><p><strong>Invited by:</strong> {{project.admin}}</p></div>
+        <button class="btn btn-accept">Accept</button>
+        <button class="btn btn-decline">Decline</button>
+
+      </div>
+    </transition-group>
   </div>
 </template>
 
@@ -25,11 +31,40 @@
     created () {
       axios.get(GET_INVITES_URL, authUser.addAuthHeader()).then( response => {
         this.projects = response.data.projects;
+        this.projects.forEach( project => {
+          project['show'] = false
+        })
       })
     }
   }
 </script>
 
 <style scoped>
-
+  .project {
+    display: inline-block;
+    border: 1px solid #eceeef;
+    border-radius: 25px;
+    margin: 10px 10px 10px 10px;
+    padding: 10px 0px 10px 0px;
+  }
+  .btn-accept {
+    background-color: #41B883;
+    color: #fff;
+    margin-right: 15px;
+    cursor: pointer;
+  }
+  .btn-decline {
+    background-color: #f44250;
+    color: #fff;
+    cursor: pointer;
+  }
+  p {
+    display: inline-block;
+  }
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0
+  }
 </style>
